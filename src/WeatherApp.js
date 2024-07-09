fa69b7a489c6bb640fca5c733708685538f329b6
jonowrenn <jw4544@columbia.edu>
2024-07-09 15:54:23 -0400

    yes

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -10,6 +10,9 @@ function WeatherApp() {
 
     try {
       const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
       const data = await response.json();
       setWeather(data);
     } catch (error) {
